3b371407d9c8641543f939fd27d7d84de1eee77d
Suzana <suzanasp1026@gmail.com>
2023-06-07 20:57:00 -0600

    first commit

---
+++ extracting_verbs.py
@@ -0,0 +1,17 @@
+import spacy
+
+with open("./phrases_used_alot.txt") as f:
+    # f.read reads the text, the first replace acounts for paragraphs, 
+    # and the second replace turn a paragraph into a one line sentence. 
+    # May not be needes for my text but it's still cool :)
+    text = f.read().replace("\n\n", " ").replace("\n", " ")
+
+nlp = spacy.load("en_core_web_sm")
+
+doc = nlp(text)
+sentences = list(doc.sents)
+first_sentence = sentences[0]
+
+# Prints each word with what is it on the side (noun, verb, aux, etc.) very cool
+for token in first_sentence:
+    print(token.text, token.pos_)
